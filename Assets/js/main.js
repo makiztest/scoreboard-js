fc623ee480a2ad791e2f546da20c64d1cfdd622f
Mark Delos Reyes <makizworks@gmail.com>
2017-10-24 21:55:16 -0400

    set winning score to players score

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -1,18 +1,30 @@
 var homeBtn = document.querySelector(".home-btn");
 var visitorBtn = document.querySelector(".visitor-btn");
-var homeScore = document.querySelector(".homeScore");
-var visitorScore = document.querySelector(".visitorScore");
-var homeInitialScore = 0;
-var visitorInitialScore = 0;
+var homeScoreDisplay = document.querySelector(".homeScore");
+var visitorScoreDisplay = document.querySelector(".visitorScore");
+var homeScore = 0;
+var visitorScore = 0;
+var gameOver = false;
+var winningScore = 5;
 
 homeBtn.addEventListener("click", function() {
     //add score to home
-    homeInitialScore++;
-    homeScore.textContent = homeInitialScore;
+    if(!gameOver) {
+        homeScore++;
+        if(homeScore === winningScore) {
+            console.log("home")
+        }
+        homeScoreDisplay.textContent = homeScore;
+    }
 });
 
 visitorBtn.addEventListener("click", function() {
     //add score to visitor
-    visitorInitialScore++;
-    visitorScore.textContent = visitorInitialScore;
+    if(!gameOver) {
+        visitorScore++;
+        if(visitorScore === winningScore) {
+            console.log("visitor")
+        }
+        visitorScoreDisplay.textContent = visitorScore;
+    }
 })
