3e1be746770dc66a3f68bacd65f02f352e9eb73b
Mark Delos Reyes <makizworks@gmail.com>
2017-10-25 00:46:01 -0400

    refactor blink function code

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -4,10 +4,10 @@ var resetBtn = document.querySelector(".reset");
 var homeScoreDisplay = document.querySelector(".homeScore");
 var visitorScoreDisplay = document.querySelector(".visitorScore");
 var winner = document.querySelector(".winner");
-var blink = false;
 var homeScore = 0;
 var visitorScore = 0;
 var gameOver = false;
+var winnerBlink;
 var winningScore = 5;
 
 homeBtn.addEventListener("click", function() {
@@ -17,8 +17,10 @@ homeBtn.addEventListener("click", function() {
         if(homeScore === winningScore) {
             winner.textContent = "home wins";
             winner.classList.add("winner");
-            winnerBlink();
             gameOver = true;
+            winnerBlink = setInterval(function() {
+                winner.classList.toggle("winnerBlink");
+            }, 1000)
         }
         homeScoreDisplay.textContent = homeScore;
     }
@@ -31,27 +33,25 @@ visitorBtn.addEventListener("click", function() {
         if(visitorScore === winningScore) {
             winner.textContent = "visitor wins";
             winner.classList.add("winner");
-            winnerBlink();
             gameOver = true;
+            winnerBlink = setInterval(function() {
+                winner.classList.toggle("winnerBlink");
+            }, 1000)
         }
         visitorScoreDisplay.textContent = visitorScore;
     }
 })
 
-function winnerBlink() {
-    if(!blink) {
-        setInterval(function() {
-            winner.classList.toggle("winnerBlink");
-        }, 800)
-    }
+var stopBlink = function() {
+    clearInterval(winnerBlink);
 }
-
-
 
 resetBtn.addEventListener("click", function() {
     homeScore = 0;
     visitorScore = 0;
     homeScoreDisplay.textContent = homeScore;
     visitorScoreDisplay.textContent = visitorScore;
+    winner.textContent = "";
+    stopBlink();
     gameOver = false;
 })
